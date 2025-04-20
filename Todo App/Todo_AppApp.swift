1361c8635e264e84e37b5e3ca662d74cf66375a5
Zongwei Tang <tangzongwei070509@gmail.com>
2025-04-20 22:32:14 +0800

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Citrus Furina on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todo AppTests/Todo_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppTests.swift
+//  Todo AppTests
+//
+//  Created by Citrus Furina on 4/20/25.
+//
+
+import Testing
+@testable import Todo_App
+
+struct Todo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
